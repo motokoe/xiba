d852f874a528e04af1f0bee2806faa8cfaeb572e
motokoe <92364312+motokoe@users.noreply.github.com>
2023-03-23 13:09:17 +0800

    Update DailyCheckIn.sol

--- DailyCheckIn.sol
+++ DailyCheckIn.sol
@@ -1,8 +1,15 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AIYYToken.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+// AIYYToken
+contract AIYYToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("AIYYToken", "AIYY") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+// CheckIn
 contract CheckIn {
     AIYYToken private _token;
     uint256 private _checkInReward = 100000000000000000;
